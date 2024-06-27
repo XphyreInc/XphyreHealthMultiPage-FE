ab6733dfa362724d329b54fbf854340faad056ec
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2024-06-27 23:48:48 +0500

    read more and less done

--- src/components/IconBox/IconBoxStyle4.jsx
+++ src/components/IconBox/IconBoxStyle4.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import { heroBanner } from '../../assets/allAssets';
+import React, { useState } from 'react';
 
+export default function IconBoxStyle4({ title, description }) {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-export default function IconBoxStyle4({ title, description}) {
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const getShortDescription = (text, wordLimit) => {
+    const words = text.split(' ');
+    if (words.length > wordLimit) {
+      return words.slice(0, wordLimit).join(' ') + '...';
+    }
+    return text;
+  };
+
+  const wordLimit = 3;
+  const shortDescription = getShortDescription(description, wordLimit);
+
   return (
     <div className="cs_iconbox cs_style_4">
       <h2 className="cs_iconbox_title cs_fs_32">{title}</h2>
-      <p className="cs_iconbox_subtitle m-0">{description}</p>
+      <div onClick={toggleExpand}>
+        <p className="cs_iconbox_subtitle m-0" >
+          {isExpanded ? description : shortDescription}
+        </p>
+        <button style={{ border: "none", borderRadius: "0.5rem" }} onClick={toggleExpand}>
+          {isExpanded ? 'Read Less' : 'Read More'}
+        </button>
+      </div>
     </div>
   );
 }
