c7bc9276372217c02c0fe0bf207591588f05648d
muhammadanoosh <muhammadanoosh.ma@gmail.com>
2024-08-23 00:43:57 +0500

    ...

--- src/Constants/constants.jsx
+++ src/Constants/constants.jsx
@@ -385,7 +385,7 @@ export const C_and_C_Data2 = [
             What's more? We utilize the latest technology to mitigate the risk of costly errors while enhancing your turnaround times.`
     },
     {
-        title: 'Your Pathway to Simplicity: How to Get Started with Xphyre Health',
+        title: 'Your Pathway to Simplicity',
         description:
             `Empower your practice with top-tier credentialing and contracting services by partnering with Xphyre Health today! By reaching out, you can learn more about how we help you navigate credentialing and contracting challenges.`
     },
